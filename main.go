490000ea466654105d93c5c748f5eee1924b575f
pukar <pukarlamichhane567@gmail.com>
2024-12-18 09:58:24 +0545

    add delete function

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 type store struct {
 	data map[string]string
@@ -11,12 +14,31 @@ func (s *store) set(key, value string) {
 }
 
 func (s *store) get(key string) string {
-	return s.data[key]
+	if value, ok := s.data[key]; ok {
+		return value
+	}
+	return "Data not found"
+}
+
+func (s *store) del(key string) {
+	delete(s.data, key)
 }
 
 func main() {
 
-	s := store{data: make(map[string]string)}
+	starttime := time.Now()
+	s := &store{data: make(map[string]string)}
 	s.set("foo", "boo")
-	fmt.Println(s.get("foo"))
+	s.set("foo1", "boo1")
+	s.set("foo2", "boo2")
+	s.set("foo3", "boo3")
+	s.set("foo4", "boo4")
+	s.set("foo5", "boo5")
+	endtime := time.Now()
+
+	duration := endtime.Sub(starttime)
+	fmt.Print(duration)
+	fmt.Println(s.get("foo5"))
+	s.del("foo3")
+	fmt.Println(s.get("3"))
 }
